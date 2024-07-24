ca31f76f0e9ce3b33d9f94497bc56291c70eb866
Valeriya <Dzhavadova.v@yandex.ru>
2024-07-24 17:38:29 +0300

    fix: saving in json format

--- app/file_helpers.py
+++ app/file_helpers.py
@@ -2,14 +2,17 @@ import json
 import os
 
 
-def save(data, file_path: str) -> None:
-    with open(file_path, "a", encoding="utf-8") as file:
-        json.dump(data, file, ensure_ascii=False, indent=2)
-
-
-def load_data(file_path: str) -> list[str]:
+def load_data(file_path: str) -> dict:
     if os.path.exists(file_path):
         with open(file_path, encoding="utf-8") as file:
             return json.load(file)
     else:
-        return []
+        return {"books": []}
+
+
+def save(data, file_path: str) -> None:
+    existed_data = load_data(file_path)
+    existed_data["books"].append(data)
+
+    with open(file_path, "w", encoding="utf-8") as file:
+        json.dump(existed_data, file, ensure_ascii=False, indent=2)
