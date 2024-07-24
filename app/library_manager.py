21026ecf24993a490742900cae7d790fc459573f
Valeriya <Dzhavadova.v@yandex.ru>
2024-07-24 19:05:14 +0300

    create Librarian

---
+++ app/library_manager.py
@@ -0,0 +1,56 @@
+from file_helpers import save, load_data, update
+from models import Book
+
+
+class Librarian:
+
+    def __init__(self) -> None:
+        self.path = "library.json"
+
+    def _save_book(self, book: dict[str, str]):
+        save(book, self.path)
+
+    def _load_books(self):
+        return load_data(self.path)
+
+    def _update_books(self, books: dict[str, str]):
+        return update(books, self.path)
+
+    def _get_by_id(self, id: str) -> int | None:
+        books = self._load_books()["books"]
+        for i, book in enumerate(books):
+            if book["id"] == id:
+                return i
+
+    def add_book(self, title: str, author: str, year: str) -> None:
+        new_book = Book(title, author, year)
+        try:
+            new_book_dict = vars(new_book)
+            self._save_book(new_book_dict)
+
+            print(f"\nBook has been saved successfully.\nID: {new_book_dict['id']}")
+        except Exception as e:
+            print(f"Error saving the book: {e}")
+
+    def delete_book(self, id: str) -> None:
+        del_book = self._get_by_id(id)
+        if del_book:
+            try:
+                books = self._load_books()["books"]
+                books.pop(del_book)
+                self._update_books(books)
+
+                print(f"\nBook {id} has been deleted successfully.")
+            except Exception as e:
+                print(f"Error deleting the book: {e}")
+        else:
+            print(f"\nThere is no book with id: {id}")
+
+
+l = Librarian()
+
+l.add_book("1", "A", "12344")
+l.add_book("2", "B", "12344")
+l.delete_book("59175f94-2024-07-24")
+l.add_book("34", "Bфф", "12344")
+l.delete_book("59175f94-2024-07-24")
